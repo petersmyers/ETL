21f5236767570357530d7d2c29fa4f2aa6f7e9a6
PSMyers <petersmyers@wustl.edu>
2019-04-09 20:17:53 -0500

    loaded data correctly

--- SQLData.sql
+++ SQLData.sql
@@ -1,37 +1,37 @@
-DROP DATABASE IF EXISTS parkd_db;
+DROP DATABASE IF EXISTS park_db;
 CREATE DATABASE park_db;
 
 USE park_db;
-CREATE TABLE parks_attendance(
-ID INT AUTO_INCREMENT PRIMARY KEY,
-Park VARCHAR(50),
-January INT,
-February INT,
-March INT,
-April INT,
-May INT,
-June INT,
-July INT,
-August INT,
-September INT,
-October INT,
-November INT,
-December INT,
-);
+-- CREATE TABLE park_attendance(
+-- ID INT AUTO_INCREMENT PRIMARY KEY,
+-- Park VARCHAR(50),
+-- January INT,
+-- February INT,
+-- March INT,
+-- April INT,
+-- May INT,
+-- June INT,
+-- July INT,
+-- August INT,
+-- September INT,
+-- October INT,
+-- November INT,
+-- December INT
+-- );
 
-CREATE TABLE parks_traffic(
-ID INT AUTO_INCREMENT PRIMARY KEY,
-Park VARCHAR(50),
-January INT,
-February INT,
-March INT,
-April INT,
-May INT,
-June INT,
-July INT,
-August INT,
-September INT,
-October INT,
-November INT,
-December INT,
-);
+-- CREATE TABLE park_traffic(
+-- ID INT AUTO_INCREMENT PRIMARY KEY,
+-- Park VARCHAR(50),
+-- January INT,
+-- February INT,
+-- March INT,
+-- April INT,
+-- May INT,
+-- June INT,
+-- July INT,
+-- August INT,
+-- September INT,
+-- October INT,
+-- November INT,
+-- December INT
+-- );
